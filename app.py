950783e3caaeeae1c7920d67ba135a13c3d8b1c0
YuanhuiXu <xuuuyuanhui@foxmail.com>
2024-12-05 14:53:48 +0000

    chore: configuration

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
 @app.route("/")
 def home():
     return render_template("index.html")
